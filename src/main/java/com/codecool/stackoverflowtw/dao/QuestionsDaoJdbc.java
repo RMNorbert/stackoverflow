59acfbaf1726153064720c9ebe35d4699dfe70e6
Norbert Remenyi <remenyinorbert@gmail.com>
2023-03-27 14:48:41 +0200

    add sql file to create tables

--- src/main/java/com/codecool/stackoverflowtw/dao/QuestionsDaoJdbc.java
+++ src/main/java/com/codecool/stackoverflowtw/dao/QuestionsDaoJdbc.java
@@ -1,6 +1,13 @@
 package com.codecool.stackoverflowtw.dao;
 
+import java.sql.Connection;
+
 public class QuestionsDaoJdbc implements QuestionsDAO {
+
+/*    public Connection connectToDatabase(Database database){
+        databaseConnection = database.getConnection();
+        return databaseConnection;
+    }*/
     @Override
     public void sayHi() {
         System.out.println("Hi DAO!");
