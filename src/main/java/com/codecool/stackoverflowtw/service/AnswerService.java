c6f4a7456948e7852658b0e8b8f7dbb52696b659
Dániel Pintér <daniel.pinter02@edu.bme.hu>
2023-03-28 12:42:20 +0200

    refactor: make service functions smaller

--- src/main/java/com/codecool/stackoverflowtw/service/AnswerService.java
+++ src/main/java/com/codecool/stackoverflowtw/service/AnswerService.java
@@ -2,12 +2,10 @@ package com.codecool.stackoverflowtw.service;
 
 import com.codecool.stackoverflowtw.controller.dto.answer.AnswerDTO;
 import com.codecool.stackoverflowtw.controller.dto.answer.NewAnswerDTO;
-import com.codecool.stackoverflowtw.dao.model.answer.Answer;
 import com.codecool.stackoverflowtw.dao.model.answer.AnswerDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,20 +20,17 @@ public class AnswerService {
 
     public List<AnswerDTO> getAllAnswers() {
         // TODO:
-        List<AnswerDTO> answerDTOS = new ArrayList<>();
-        answerDAO.getAllAnswers().forEach(answer -> answerDTOS.add(new AnswerDTO(answer)));
-        return answerDTOS;
+        return answerDAO.getAllAnswers().stream().map(AnswerDTO::new).toList();
     }
 
     public Optional<AnswerDTO> getAnswerById(int id) {
         // TODO
         answerDAO.sayHi();
-        Optional<Answer> answer = answerDAO.findAnswerById(id);
-
+// answer = answerDAO.findAnswerById(id)
 //        if (answer.isPresent()) {
 //            return Optional.of(new AnswerDTO(answer.get()));
 //        } else return Optional.empty();
-        return answer.map(AnswerDTO::new); //apparently the same as above
+        return answerDAO.findAnswerById(id).map(AnswerDTO::new); //apparently the same as above
     }
 
     public boolean deleteAnswerById(int id) {
