fcba6a96e26cf2af712e8f4846096a312944c481
Dániel Pintér <daniel.pinter02@edu.bme.hu>
2023-03-28 15:45:20 +0200

    style: remove imports

--- frontend/stackoverflow++/src/App.jsx
+++ frontend/stackoverflow++/src/App.jsx
@@ -1,8 +1,5 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import { Home } from "./components/Home";
 import { Header } from "./components/Header";
 
 function App() {
