e3488ad83cfa1f3b24631c2071a8c801ca7a96b1
Dániel Pintér <daniel.pinter02@edu.bme.hu>
2023-03-30 11:19:31 +0200

    feat: make neccessary component bases

---
+++ frontend/stackoverflow++/src/components/answers/Answer.jsx
@@ -0,0 +1,5 @@
+export const Answer = ()=>{
+    return(
+        <div></div>
+    )
+}
---
+++ frontend/stackoverflow++/src/components/user/Register.jsx
@@ -0,0 +1,47 @@
+import { useNavigate } from "react-router-dom";
+import { checkAvailableUser } from "../../utils/checkAvailableuser";
+import { createUser } from "../../utils/createUser";
+
+export const Register = () => {
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let username = e.target[0].value;
+    let password = e.target[1].value;
+    console.log(password);
+    //Post request, hogy van-e ilyen felhasznalo
+    let user = await checkAvailableUser(username, password);
+    console.log(user);
+    if (user) console.log("This exists");
+    else {
+    //   createUser(username, password);
+    }
+
+    // navigate("/");
+  };
+  return (
+    <div>
+      <div>Register!</div>
+      <div className="flex justify-center flex-col items-center text-2xl ">
+        <form
+          action=""
+          onSubmit={(e) => {
+            handleSubmit(e);
+          }}
+          className="flex justify-center flex-col items-center text-black"
+        >
+          <label htmlFor="name">Username</label>
+          <input type="text" className="name" />
+          <label htmlFor="">Password</label>
+          <input type="password" className="password" />
+          <button
+            type="submit"
+            className="bg-buttonBlue m-5 p-2 border-spacing-2 border-black border-4 hover:bg-blue-600"
+          >
+            Register
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
